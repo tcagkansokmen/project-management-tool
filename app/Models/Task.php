a6324049500afb1549b84488250dcf6764be2ad9
Tufan Çağkan Sökmen <tcagkan.sokmen@gmail.com>
2024-10-04 18:13:48 +0300

    added Task entity

---
+++ app/Models/Task.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'name',
+        'description',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => TaskStatus::class,
+    ];
+
+    /**
+     * Get the project that owns the task.
+     *
+     * @return BelongsTo
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
